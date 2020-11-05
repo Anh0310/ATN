ab6e41e671ab0f77dc4511ce4070b067f266962b
Anh0310 <66456807+Anh0310@users.noreply.github.com>
2020-11-05 09:13:27 +0700

    Update config.php

--- ANT/config.php
+++ ANT/config.php
@@ -1,10 +1,10 @@
 <?php 
     function getDB() {
-        return $db = pg_connect("host=ec2-34-200-72-77.compute-1.amazonaws.com
-                          dbname=dbi4mim1e236mc
+        return $db = pg_connect("host=ec2-100-25-100-81.compute-1.amazonaws.com
+                          dbname=d8kr7qj91p3pam
                           port=5432
-                          user=brwuaeapwdymll
-                          password=3ff1daf0cf7573f885bb9e54db192f6f947c4a46a656d850339375b70fac91ab")
+                          user=wpjvqdtioqsteh
+                          password=fc0dae1d91fba04a4b03b1d140a7291a1db7cd1238193abf1101f6297bcf5fd4")
                           or die("Connection failed!")
     }
 
